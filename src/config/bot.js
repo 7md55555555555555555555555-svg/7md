d400e692f79b09e6f0f6149a5c29d2e0fbc92846
7md55555555555555555555555-svg <7md55555555555555555555555@gmail.com>
2026-08-09 02:36:42 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "الشافعي", // required by Discord API, not shown in the client
         state: "فإن كانت الأيام خانت عهودنا فإني بها راضٍ ولكنها قهر",     // this is what people actually see
         type: 2,               // Custom
       },
